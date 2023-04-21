daa3f9bb3009d67503a7015c448cca5c68516a30
Ranjan <73215366+ranjan2829@users.noreply.github.com>
2023-04-21 17:12:58 +0000

    code

---
+++ Odd Even Linked LIst/code.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null) return null;
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = head.next;
+        // `even != null` rules out the list of only 1 node
+        // `even.next != null` rules out the list of only 2 nodes
+        while (even != null && even.next != null) {
+            // Put odd to the odd list
+            odd.next = odd.next.next;
+            
+            // Put even to the even list
+            even.next = even.next.next;
+            
+            // Move the pointer to the next odd/even
+            odd = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+}
