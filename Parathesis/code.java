7da07c755ab46b9948d4822b9b51efe58a8b13de
Ranjan <73215366+ranjan2829@users.noreply.github.com>
2023-07-25 16:12:11 +0000

    code

---
+++ Parathesis/code.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack=new Stack<Character>();
+        for(char c: s.toCharArray()){
+            if(c=='('||c=='{'||c=='['){
+                stack.push(c);
+            }
+            else{
+                if(stack.isEmpty()){
+                    return false;
+                }
+                char top=stack.peek();
+            }
+
+        }
+
+        
+    }
+}
