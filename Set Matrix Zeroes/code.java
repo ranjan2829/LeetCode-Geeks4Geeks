457aa6832dd54efa91bd172c2925c8310494ee9e
Ranjan <73215366+ranjan2829@users.noreply.github.com>
2023-07-27 12:52:13 +0000

    done

--- Set Matrix Zeroes/code.java
+++ Set Matrix Zeroes/code.java
@@ -7,10 +7,18 @@ class Solution {
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 if (matrix[i][j] == 0) {
-                    zeroRows[i] = true;
-                    zeroCols[j] = true;
+                    zeror[i] = true;
+                    zeroc[j] = true;
+                }
+            }
+        }
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (zeroRows[i] || zeroCols[j]) {
+                    matrix[i][j] = 0;
                 }
             }
         }
 
     }
+}
