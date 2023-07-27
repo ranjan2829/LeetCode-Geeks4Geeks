626df1efa864547f4a389d1f987e9b73d98a3d60
Ranjan <73215366+ranjan2829@users.noreply.github.com>
2023-07-27 17:01:02 +0000

    done

---  Rotate Image/code.java
+++  Rotate Image/code.java
@@ -1,14 +1,6 @@
 class solution{
     public void rotate(int[][] matrix){
-        int n=matrix.length;
-        for(int i=0;i<n;i++){
-            int a=0;
-            int b=n-1;
-            while(a<=b){
-                int temp=matrix[a][i];
-                matrix[b][i]=temp;
-                a++;
-                b--;
+        
 
             }
         }
