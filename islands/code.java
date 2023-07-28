132176cbf80bc28b4eb36d5b8377cbb49cddf5b2
Ranjan <73215366+ranjan2829@users.noreply.github.com>
2023-07-28 19:07:27 +0000

    done

--- islands/code.java
+++ islands/code.java
@@ -6,15 +6,29 @@ class Solution {
         int count=0;
         for(int i =0;i<row;i++){
             for(int j=0;j<col;j++){
-                if(grid[i][j]=='1' && search()){
+                if(grid[i][j]=='1'){
                     count=count+1;
+                    search(i,j,grid);
                 }
             }
         }
         return count;
         
     }
-    private boolean search(int i,int j, int idx, char[][] grid){
+    //dfs backtrackig algortihm
+    private void search(int i,int j,char[][] grid){
+        if(i<0||i>=grid.length||j<0||j>=grid[i].length||grid[i][j]=='0'){
+            return;
+        }
+        grid[i][j]='0';
+        search(i+1, j, grid);
+        search(i-1, j, grid);
+        search(i, j-1, grid);
+        search(i, j+1, grid);
         
+
+
+        
+
     }
 }
