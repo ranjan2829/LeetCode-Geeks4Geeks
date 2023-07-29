347a4d6c9bf6a31dabcf49070af560ef66530419
Ranjan <73215366+ranjan2829@users.noreply.github.com>
2023-07-29 06:43:11 +0000

    done

---  Surrounded Regions/code.java
+++  Surrounded Regions/code.java
@@ -1,11 +1,14 @@
 class Solution {
     public void solve(char[][] board) {
+        if(board.length==0||board[0].length==0){
+            return;
+        }
         int row=board.length;
         int col=board[0].length;
         boolean visited[][] = new boolean[row][col];
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
-                if(board[i][j]=='O' && searchdfs(i,j,idx,board)){
+                if(board[i][j]=='O' && searchdfs(i,j,board)){
                     board[i][j]='X';
 
                 }
@@ -13,15 +16,15 @@ class Solution {
         }
         
     }
-    private boolean searchdfs(int i, int j , int idx, char[][] board){
+    private boolean searchdfs(int i, int j , char[][] board){
         if(i<0||i>=board.length||j<0||j>=board[i].length||visited[i][j]){
             return false;
         }
         visited[i][j]=true;
-        if(searchdfs(i+1, j, idx, board)||
-           searchdfs(i-1, j, idx, board)||
-           searchdfs(i, j+1, idx, board)||
-           searchdfs(i, j-1, idx, board))
+        if(searchdfs(i+1, j, board)||
+           searchdfs(i-1, j, board)||
+           searchdfs(i, j+1, board)||
+           searchdfs(i, j-1, board))
         {
             return true;
         }
