a4159f6e6b6a8104273ff6e79bda4adc5505d578
Ranjan <73215366+ranjan2829@users.noreply.github.com>
2023-07-29 07:11:05 +0000

    done

---  Surrounded Regions/code.java
+++  Surrounded Regions/code.java
@@ -38,12 +38,24 @@ class Solution {
         }
         
     }
-    private void boundarydfs(int i, int j , char[][] board){
+    public void boundarydfs(int i, int j , char[][] board){
         if(i<0||i>board.length||j<0||j>=board[0].length){
             return;
         }
-        if(board[i][j]=='*'){
-            board[i][j]='O';
+        if(board[i][j]=='O'){
+            board[i][j]='*';
+        }
+        if(i>0 && board[i-1][j]=='O'){
+            boundarydfs(i-1, j, board);
+        }
+        if(i<board.length-1 && board[i+1][j]=='O'){
+            boundarydfs(i+1, j, board);
+        }
+        if(j>0 && board[i][j-1]=='O'){
+            boundarydfs(i, j-1, board);
+        }
+        if(j<board[0].length-1 && board[i][j+1]=='O'){
+            boundarydfs(i, j+1, board);
         }
 
     }
