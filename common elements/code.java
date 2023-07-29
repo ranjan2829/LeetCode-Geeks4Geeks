77845f406beea2b059018a9a8dc311f6fa8311e5
Ranjan <73215366+ranjan2829@users.noreply.github.com>
2023-07-29 15:25:36 +0000

    done

---
+++ common elements/code.java
@@ -0,0 +1,16 @@
+class Solution
+{
+    ArrayList<Integer> commonElements(int A[], int B[], int C[], int n1, int n2, int n3) 
+    
+    {
+        ArrayList<Integer> ans=new ArrayList<>();
+        for(int i=0;i<n1;i++){
+            for(int j=0;j<n2;j++){
+                for(int k=0;k<n3;k++){
+                    
+                }
+            }
+        }
+        // code here 
+    }
+}
