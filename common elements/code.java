3bdbd2a60a704ed2ed57014cf56d6bd47497703a
Ranjan <73215366+ranjan2829@users.noreply.github.com>
2023-07-29 15:26:48 +0000

    done

--- common elements/code.java
+++ common elements/code.java
@@ -7,10 +7,14 @@ class Solution
         for(int i=0;i<n1;i++){
             for(int j=0;j<n2;j++){
                 for(int k=0;k<n3;k++){
+                    if(A[i]==B[j]==C[k]){
+                        ans.add(A[i]);
+                    }
                     
                 }
             }
         }
+        return ans;
         // code here 
     }
 }
