0be76d765a4974affe4957d2c3a92d539e6095df
Ranjan <73215366+ranjan2829@users.noreply.github.com>
2023-07-30 04:48:36 +0000

    done

--- common elements/code.java
+++ common elements/code.java
@@ -7,7 +7,7 @@ class Solution
         for(int i=0;i<n1;i++){
             for(int j=0;j<n2;j++){
                 for(int k=0;k<n3;k++){
-                    if(A[i]==B[j]==C[k]){
+                    if(A[i]==B[j] && B[j] ==C[k]){
                         ans.add(A[i]);
                     }
                     
