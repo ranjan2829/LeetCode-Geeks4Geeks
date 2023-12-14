1e2d669416737a2d1c30b5c2bd42b5e769237e8a
Ranjan <73215366+ranjan2829@users.noreply.github.com>
2023-12-14 06:43:54 +0000

    done

---
+++ zigzag/code.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String convert(String s, int numRows) {
+        if(numRows==1||numRows>s.length()){
+            return s;
+        }
+        char[][] board=new char[n][n];
+        int row=0;
+        int col=0;
+        boolean down=false;
+        for(char c:s.toCharArray()){
+            board[row][col]=c;
+
+        }
+
+        
+        
+    }
+}
