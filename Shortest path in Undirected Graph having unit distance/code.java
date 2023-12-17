63d3307a51f28aa97fee18ec157d0b151e4c45cc
Ranjan <73215366+ranjan2829@users.noreply.github.com>
2023-12-17 17:56:38 +0000

    done

--- Shortest path in Undirected Graph having unit distance/code.java
+++ Shortest path in Undirected Graph having unit distance/code.java
@@ -1,6 +1,14 @@
+class Pair{
+    int node;
+    int dist;
+    Pair(int s,int n){
+        this.node=n;
+        this.dist=s;
+    }
+}
 class Solution {
     
     public int[] shortestPath(int[][] edges,int n,int m ,int src) {
-        // Code here
+        PriorityQueue pq=new PriorityQueue((x,y)->)
     }
 }
